3fb86e89ef8f61986b79b06a0be73f5b540c3a7e
harbinailin <harbinailin@outlook.com>
2022-03-15 15:13:13 +0800

    leetcode 两数之和

---
+++ leetcode/1_两数之和.py
@@ -0,0 +1,9 @@
+# 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
